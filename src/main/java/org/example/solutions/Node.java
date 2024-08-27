849e76bd1079cb75d1cb6b8cb73f5b468a767082
szolsza <szolsza@gmail.com>
2024-08-27 21:03:46 +0200

    Linked list part 3

--- src/main/java/org/example/solutions/Node.java
+++ src/main/java/org/example/solutions/Node.java
@@ -1,6 +1,7 @@
 package org.example.solutions;
 
 class Node<T> {
+
     T val;
     Node<T> next;
 
@@ -9,10 +10,17 @@ class Node<T> {
         this.next = null;
     }
 
+    public Node(T val, Node<T> next) {
+        this.val = val;
+        this.next = next;
+    }
+
     public static <T> void printNode(Node<T> node) {
+        System.out.println();
         while (node != null) {
             System.out.print(node.val + "\t");
             node = node.next;
         }
+        System.out.println();
     }
 }
