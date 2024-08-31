906a00a1cc8ea4e450ffa4a58ef45cdb2f792571
Szymon Olsza <szolsza@gmail.com>
2024-08-31 20:03:46 +0200

    fix main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
-import org.example.solutions.DefaultSolution;
-import org.example.solutions.dynamic.Tribonacci;
-
 public class Main {
     public static void main(String[] args) {
-        DefaultSolution defaultSolution = new Tribonacci();
-        defaultSolution.runDefaultExample();
     }
 }
