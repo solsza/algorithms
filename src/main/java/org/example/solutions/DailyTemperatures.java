8846a1c84e31c8c0a69849df2ba5514b91fcaaaa
szymon.olsza <szolsza@gmail.com>
2025-07-06 21:00:20 +0200

    Add daily temperatures

---
+++ src/main/java/org/example/solutions/DailyTemperatures.java
@@ -0,0 +1,26 @@
+package org.example.solutions;
+
+import java.util.Stack;
+
+public class DailyTemperatures implements DefaultSolution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>(); // pair: [temp, index]
+
+        for (int i = 0; i < temperatures.length; i++) {
+            int t = temperatures[i];
+            while (!stack.isEmpty() && t > stack.peek()[0]) {
+                int[] pair = stack.pop();
+                res[pair[1]] = i - pair[1];
+            }
+            stack.push(new int[]{t, i});
+        }
+        return res;
+    }
+
+    @Override
+    public void runDefaultExample() {
+
+    }
+}
